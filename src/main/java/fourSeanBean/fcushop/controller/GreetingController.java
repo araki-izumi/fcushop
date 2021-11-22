c33ffa5542d18c7ed19b1ff094d86053efc5fd8f
Izumi <71806015+araki-izumi@users.noreply.github.com>
2021-11-22 11:15:01 +0800

    first commit

--- src/main/java/fourSeanBean/fcushop/controller/GreetingController.java
+++ src/main/java/fourSeanBean/fcushop/controller/GreetingController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
     @GetMapping("/greeting")
     public String sayHello(@RequestParam String name) {
-        return "Hello " + name;
+        return "Hi " + name;
     }
 }
