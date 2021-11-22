5a11c08a3a8c53f98e8929d82bb9cb57add43720
araki-izumi <fairy900326@gmail.com>
2021-11-22 11:05:35 +0800

    init commit

---
+++ src/main/java/fourSeanBean/fcushop/FcushopApplication.java
@@ -0,0 +1,14 @@
+package fourSeanBean.fcushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class FcushopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcushopApplication.class, args);
+	}
+
+}
